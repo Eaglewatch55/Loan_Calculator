ed0513593deb5231b9cefb3fe82aae5df6441db5
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-03-10 21:28:54 -0600

    Stage 4/4

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,29 +1,62 @@
+import argparse
 from math import ceil, log
 
-print("What do you want to calculate?")
-print("type \"n\" - for number of monthly payments,")
-print("type \"a\" - for annuity monthly payment amount,")
-print("type \"p\" - for loan principal:")
-calculation_type = input()
+parser = argparse.ArgumentParser(description="This program calculates the loan payments")
+parser.add_argument("--type")
+parser.add_argument("--payment")
+parser.add_argument("--principal")
+parser.add_argument("--periods")
+parser.add_argument("--interest")
+args = parser.parse_args()
 
-input_messages = {"principal" : "Enter the loan principal:\n",
-                  "periods": "Enter the number of periods:\n",
-                  "annuity": "Enter the monthly payment:\n",
-                  "interest": "Enter the loan interest:\n"}
+calculation_type = args.type
+payment = float(args.payment) if args.payment is not None else None
+principal = float(args.principal) if args.principal is not None else None
+periods = float(args.periods) if args.periods is not None else None
+interest = float(args.interest) if args.interest is not None else None
+
+args_dict = {"type": calculation_type,
+             "payment": payment,
+             "principal": principal,
+             "periods": periods,
+             "interest": interest}
+
+
+def args_validation(val_dict: dict):
+
+    if not (val_dict["type"] == "annuity" or val_dict["type"] == "diff"):
+        print("Invalid type")
+        return False
+
+    if val_dict["type"] == "diff" and val_dict["payment"] is not None:
+        print("Payment with diff")
+        return False
+
+    if val_dict["interest"] is None:
+        print("Interest not given")
+        return False
+
+    counter = 0
+    not_negative = ("principal", "periods", "interest")
+    for key in val_dict.keys():
+        counter += 1 if val_dict[key] is None else 0
+
+        if counter > 1:
+            print("Less than 4 arguments")
+            return False
+
+        if key in not_negative and val_dict[key] is not None and val_dict[key] < 0:
+            print("Negative value")
+            return False
+
+    return True
 
 
 def monthly_interest(i):
     return i / (12 * 100)
 
 
-if calculation_type == "n":
-    p = float(input(input_messages["principal"]))
-    a = float(input(input_messages["annuity"]))
-    i = float(input(input_messages["interest"]))
-
-    i = monthly_interest(i)
-    n = ceil(log(a / (a - (i * p)), 1 + i))
-
+def period_message(n):
     years = int(n // 12)
     months = ceil(n % 12)
 
@@ -34,41 +67,51 @@ if calculation_type == "n":
                 "default": f"It will take {years} years and {months} months to repay this loan!"}
 
     if years == 0:
-        print(messages["months"])
+        return messages["months"]
 
     elif years == 1 and months == 0:
-        print(messages["one_year"])
+        return messages["one_year"]
 
     elif years == 1:
-        print(messages["one_year_months"])
+        return messages["one_year_months"]
 
     elif months == 0:
-        print(messages["years"])
+        return messages["years"]
 
     else:
-        print(messages["default"])
+        return messages["default"]
 
 
-elif calculation_type == "a":
-    p = float(input(input_messages["principal"]))
-    n = float(input(input_messages["periods"]))
-    i = float(input(input_messages["interest"]))
+if not args_validation(args_dict):
+    print("Incorrect parameters")
+    exit()
 
-    i = monthly_interest(i)
+interest = monthly_interest(interest)
 
-    a = ceil(p * (i * (1 + i) ** n) / ((1 + i) ** n - 1))
+if calculation_type == "diff":
+    total_payment = 0
+    for period in range(1, int(periods) + 1, 1):
+        d = ceil((principal / periods) + interest * (principal - (principal * (period - 1)) / periods))
+        total_payment += d
+        print(f"Month {period}: payment is {d}\n")
 
-    print(f"Your monthly payment = {a}!")
+    overpayment = total_payment - principal
 
-elif calculation_type == "p":
-    a = float(input(input_messages["annuity"]))
-    n = float(input(input_messages["periods"]))
-    i = float(input(input_messages["interest"]))
+if calculation_type == "annuity":
+    if payment is None:
+        payment = ceil(principal * (interest * (1 + interest) ** periods) / ((1 + interest) ** periods - 1))
+        overpayment = int(payment * periods - principal)
+        print(f"Your annuity payment = {payment}!")
 
-    i = monthly_interest(i)
+    elif principal is None:
+        principal = payment / ((interest * (1 + interest) ** periods) / ((1 + interest) ** periods - 1))
+        overpayment = int(payment * periods - principal)
+        print(f"Your loan principal = {int(principal)}")
 
-    p = a / ((i * (1 + i) ** n) / ((1 + i) ** n - 1))
+    elif periods is None:
+        periods = ceil(log(payment / (payment - (interest * principal)), 1 + interest))
+        print(period_message(periods))
 
-    print(f"Your loan principal = {p}!")
-else:
-    pass
+    overpayment = int(payment * periods - principal)
+
+print(f"Overpayment = {overpayment}")
